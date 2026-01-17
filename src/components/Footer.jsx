aa583aac99eef9f082e16989664d3420f7412cfb
Aftab-Ahmed-Soomro <aftab.ahmeddailyideas@gmail.com>
2026-01-17 12:04:55 +0500

    feat: Add a new `Footer` React component to display copyright information.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='bg-slate-200'>
+    <footer className='bg-slate-200 fixed bottom-0 w-full'>
       <div className='container mx-auto p-4'>
         <p className='text-center font-bold'>Coding with Aftab</p>
       </div>
